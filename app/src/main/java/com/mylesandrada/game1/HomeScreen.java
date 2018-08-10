55f5c2ef89f14da6aa9eb580fd0ed09f54773b36
monstermylo <myles.selvey@sky.com>
2018-08-10 19:38:51 +0100

    log in attempt

--- app/src/main/java/com/mylesandrada/game1/HomeScreen.java
+++ app/src/main/java/com/mylesandrada/game1/HomeScreen.java
@@ -3,8 +3,13 @@ package com.mylesandrada.game1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.AWSStartupHandler;
+import com.amazonaws.mobile.client.AWSStartupResult;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
@@ -12,6 +17,12 @@ public class HomeScreen extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.mylesandrada.game1";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AWSMobileClient.getInstance().initialize(this, new AWSStartupHandler() {
+            @Override
+            public void onComplete(AWSStartupResult awsStartupResult) {
+                Log.d("YourMainActivity", "AWSMobileClient is instantiated and you are connected to AWS!");
+            }
+        }).execute();
         setContentView(R.layout.activity_home_screen);
     }
 
