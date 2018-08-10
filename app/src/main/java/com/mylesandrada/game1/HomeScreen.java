3592b4cc67c661096d5e2f0d9afe9c610f9bb229
monstermylo <myles.selvey@sky.com>
2018-08-10 19:39:14 +0100

    Revert "log in attempt"
    This reverts commit 55f5c2ef89f14da6aa9eb580fd0ed09f54773b36.

--- app/src/main/java/com/mylesandrada/game1/HomeScreen.java
+++ app/src/main/java/com/mylesandrada/game1/HomeScreen.java
@@ -3,13 +3,8 @@ package com.mylesandrada.game1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import com.amazonaws.mobile.client.AWSMobileClient;
-import com.amazonaws.mobile.client.AWSMobileClient;
-import com.amazonaws.mobile.client.AWSStartupHandler;
-import com.amazonaws.mobile.client.AWSStartupResult;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
@@ -17,12 +12,6 @@ public class HomeScreen extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.mylesandrada.game1";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        AWSMobileClient.getInstance().initialize(this, new AWSStartupHandler() {
-            @Override
-            public void onComplete(AWSStartupResult awsStartupResult) {
-                Log.d("YourMainActivity", "AWSMobileClient is instantiated and you are connected to AWS!");
-            }
-        }).execute();
         setContentView(R.layout.activity_home_screen);
     }
 
